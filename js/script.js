afa225e405c11ea662a37c5837024789c61d3640
Gavin Gong <kingdompluto@gmail.com>
2016-05-25 19:11:01 +0800

    做两个版本吧

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+	var hoverTip = function() {
+
+		return {
+			showTip: function(data-item) {
+				$(['data-func']).
+			},
+			hideTip: function() {},
+			setTip: function(data-item) {
+				var html = "<div></div>"
+				var css = ""
+				$(['data-func']).append(html);
+			}
+		}
+	}
+});
