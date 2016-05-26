4f784950e276fa9dfe5d16438b077a2c914b11f1
Gavin Gong <kingdompluto@gmail.com>
2016-05-26 09:33:51 +0800

    基本完成侧边栏的JS效果

--- js/script.js
+++ js/script.js
@@ -1,101 +1,122 @@
-var cardCol = document.querySelector(".card");
-var cardTitles = cardCol.querySelector(".card-title");
-var cardContents = cardCol.querySelector(".card-content");
-var cardOptions = cardCol.querySelector(".card-option-item");
+/**
+ * Created by Kefen Gong on 5/26/2016.
+ */
+(function() {
+    var cardCol = document.querySelector(".card");
+    var headrCOl = document.querySelector(".header");
+    var cardTitles = cardCol.querySelector(".card-title");
+    var cardContents = cardCol.querySelector(".card-content");
+    var cardOptions = cardCol.querySelector(".card-option-item");
+    var sideNavBtn = headrCOl.querySelector('[data-func="side-toggle"]');
+    var sideNavElement = document.querySelector(".sidenav"); 
+    console.log("Hello");
 
 
 
-var hoverTip = function() {
-	return {
-		hideTip: function(tipElement) {
-			if(tipElement) {
-				tipElement.style.display = 'none';
-			}
-		},
-		showTip: function(tipElement) {
-			if(tipElement) {
-				tipElement.style.display = 'block'
-			}
-		},
-		setTip: function(element, cssClassName) {
-			var html = createElement('div');
-			html.innerHTML = element.dataset.func;
-			element.appendChild(html);
-			element.css += cssClassName;
-		},
-	}
-};
+    var hoverTip = function() {
+        return {
+            hideTip: function(tipElement) {
+                if(tipElement) {
+                    tipElement.style.display = 'none';
+                }
+            },
+            showTip: function(tipElement) {
+                if(tipElement) {
+                    tipElement.style.display = 'block'
+                }
+            },
+            setTip: function(element, cssClassName) {
+                var html = createElement('div');
+                html.innerHTML = element.dataset.func;
+                element.appendChild(html);
+                element.css += cssClassName;
+            },
+        }
+    };
 
-var HoverTip = hoverTip();
-HoverTip.
+    var HoverTip = hoverTip();
 
-var sideNav = function(btnElement,toggleElement) {
-	function sideNavHandler() {
-		var status = null;
-		var style = toggleElement.style;
-		var width = style.width;
+    var sideNav = function(btnElement,toggleElement) {
+            var status = null;
+            var style = toggleElement.style;
+            var width = style.width;
 
-		if (!status) {
-			showSide()
-		} else {
-			hideSide()
-		}
+            return {
+            	showSide: function () {
+	                style.left = 0;
+	                status = 1;
+            	},
+            	hideSide: function() {
+            		style.left = width;
+	                status = 0;
+            	},
+            	status: function() {
+            		return status;
+            	}
+            }
+    };
+    var SideNav = sideNav(sideNavBtn, sideNavElement);
+    // sideNavBtn.addEventListener("click", SideNav.showSide);
+    document.addEventListener("click", function(event) {
+    	if(1) {
+    		if(event.target ==  sideNavBtn) {
+    			SideNav.showSide();
+    		} 
+    		else if(sideNavElement.contains(event.target) || event.target == sideNavElement) {
+    			return false;
+    		} else {
+    			SideNav.hideSide();
+    		}
+    	}
+    }, true);
 
-		function showSide() {
-			style.left = 0;
-			status = 1;
-		}
-		function hideSide() {
-			style.left = width;
-			status = 0;
-		}
-	}
-
-	toggleElement.addEventListener('mouseover', sideNavHandler);
-}
-
-var dataManage = function() {
-	return {
-		setUIData: function(element) {
-			var title = element.querySelector(".card-title").innerHTML;
-			var context = element.querySelector(".card-content").innerHTML;
-			localStorage.title = context;
-		},
-		removeUIData: function(key) {
-			localStorage.removeItem(key);
-		}
-	}
-}
-var DataManage = dataManage();
+    var dataManage = function() {
+        return {
+            setUIData: function(element) {
+                var title = element.querySelector(".card-title").innerHTML;
+                var content = element.querySelector(".card-content").innerHTML;
+                localStorage.title = context;
+            },
+            removeUIData: function(key) {
+                localStorage.removeItem(key);
+            }
+        }
+    };
+    var DataManage = dataManage();
 
 
 // 设置为可编辑状态
-for(var q=0, len=cardTitles.length; q<len; q++ ) {
-	cardTitles[q].setAttribute("contenteditable","true");
-	cardContents[q].setAttribute("contenteditable","true");
-}
+    for(var q=0, len=cardTitles.length; q<len; q++ ) {
+        cardTitles[q].setAttribute("contenteditable","true");
+        cardContents[q].setAttribute("contenteditable","true");
+    }
 
 // 定义卡片方法
-var cardFunc = function() {
-	createCard: function(cardBOX) {
-		var newCard = cardBOX.cloneNode(deep);
-		newCard.querySelector(".card-title").innerHTML = '';
-		newCard.querySelector(".card-title").setAttribute("placeholder", "Title")
-		newCard.querySelector("card-content").innerHTML = '';
-		newCard.querySelector("card-content").setAttribute("placeholder", "input your code") = '';
-		cardBOX.insertBefore(newCard, cardBOX.firstChild);
-	}
-	removeCard: function(cardElement) {
-		var title = cardElement.querySelector('.card-title').innerHTML;
-		var content = cardElement.querySelector('card-content').innerHTML;
-		// Delete Card Node
-		cardElement.parentNode.removeChild(cardElement) = "";
+    var cardFunc = function() {
+        return {
+            createCard: function(cardBOX) {
+                var newCard = cardBOX.cloneNode(deep);
+                newCard.querySelector(".card-title").innerHTML = '';
+                newCard.querySelector(".card-title").setAttribute("placeholder", "Title");
+                newCard.querySelector("card-content").innerHTML = '';
+                newCard.querySelector("card-content").setAttribute("placeholder", "input your code");
+                cardBOX.insertBefore(newCard, cardBOX.firstChild);
+            },
+            removeCard: function(cardElement) {
+                var title = cardElement.querySelector('.card-title').innerHTML;
+                var content = cardElement.querySelector('card-content').innerHTML;
+                // Delete Card Node
+                cardElement.parentNode.removeChild(cardElement);
 
-		// 清除存储数据
-		DataManage.removeItem(title);
+                // 清除存储数据
+                DataManage.removeItem(title);
 
-	}
-}
+            }
+        }
+
+    }
 
 
 
+
+})()
---
+++ js/script_jq.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+	var hoverTip = function() {
+
+		return {
+			showTip: function(data-item) {
+				$(['data-func']).
+			},
+			hideTip: function() {},
+			setTip: function(data-item) {
+				var html = "<div></div>"
+				// var css = "";
+				data-item.append(html);
+				$("[data-attr]>div:last-child").addClass();
+			}
+		}
+	}
+});
